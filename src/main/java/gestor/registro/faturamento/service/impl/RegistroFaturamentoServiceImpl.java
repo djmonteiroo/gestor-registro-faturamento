dc833bab87612b28acfe690e92663c9bd6c4ecf7
Diego Monteiro <djmonteiroo32@gmail.com>
2024-05-20 11:21:14 -0300

    Ajuste faturamento

--- src/main/java/gestor/registro/faturamento/service/impl/RegistroFaturamentoServiceImpl.java
+++ src/main/java/gestor/registro/faturamento/service/impl/RegistroFaturamentoServiceImpl.java
@@ -3,12 +3,16 @@ package gestor.registro.faturamento.service.impl;
 import gestor.registro.faturamento.respository.DadoFaturamentoRepository;
 import gestor.registro.faturamento.respository.ProjetoRepository;
 import gestor.registro.faturamento.service.RegistroFaturamentoService;
+import gestor.registro.lib.dto.ParcelaDto;
 import gestor.registro.lib.entity.DadoFaturamento;
 import gestor.registro.lib.records.DadoFaturamentoRecord;
 import gestor.registro.lib.utils.GestaoProjetoUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.IntStream;
 
 @Slf4j
 @Service
@@ -23,7 +27,27 @@ public class RegistroFaturamentoServiceImpl implements RegistroFaturamentoServic
     public void gerarParcela(final DadoFaturamentoRecord dadoFaturamentoRecord) {
         log.info(GestaoProjetoUtils.getMensagem("Gerando parcelas para dado faturamento ", dadoFaturamentoRecord.idDadoFaturamento()));
         DadoFaturamento dadoFaturamento =  dadoFaturamentoRepository.findById(dadoFaturamentoRecord.idDadoFaturamento()).orElseThrow(() -> new RuntimeException(GestaoProjetoUtils.getMensagem("Dado de faturamento não encontrado na base")));
+        gerarValorParcela(dadoFaturamento);
 
-        System.out.println(dadoFaturamento.getProjeto());
+    }
+
+    private void gerarValorParcela(final DadoFaturamento dadoFaturamento) {
+        List<ParcelaDto> lsParcela = new ArrayList<>();
+        for(Integer nrParcela = 1; nrParcela <= dadoFaturamento.getQtParcela(); nrParcela++){
+            ParcelaDto parcelaDto = new ParcelaDto();
+            double resultado = 0;
+
+            if (nrParcela < dadoFaturamento.getQtParcela()) {
+                resultado = Math.floor((100.0 / dadoFaturamento.getQtParcela()) * 100) / 100; // TRUNCAR(100%/6;2)
+                parcelaDto.setPercentualParcela(resultado);
+            } else if (nrParcela == dadoFaturamento.getQtParcela()) {
+                resultado = Math.floor((100.0 / dadoFaturamento.getQtParcela()) * 100) / 100; // TRUNCAR(100%/6;2)
+                parcelaDto.setPercentualParcela(resultado);
+            } else {
+                resultado = 0;
+                parcelaDto.setPercentualParcela(resultado);
+            }
+            lsParcela.add(parcelaDto);
+        }
     }
 }
